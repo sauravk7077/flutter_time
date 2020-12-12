39ace85f02de3e8c8089a17a94bdd1a5cd912763
Saurav Kumar <sauravk7077@gmail.com>
2020-12-12 10:00:55 +0530

    First code

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("The King"),
+      ),
+      body: Container(
+        child: Text("Hello"),
+      ),
+    );
+  }
+}
